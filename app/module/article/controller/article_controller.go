8e18aafe3007cbd8c8c323ea2774f60595baeeef
sanbei101 <sanbei101@outlook.com>
2025-07-08 17:21:10 +0800

    精简目录结构

--- app/module/article/controller/article_controller.go
+++ app/module/article/controller/article_controller.go
@@ -8,7 +8,6 @@ import (
 	"github.com/gofiber/fiber/v2"
 )
 
-// Requests & responses for ArticleController & ArticleService
 type ArticleRequest struct {
 	Title   string `json:"title" form:"title" validate:"required,max=255"`
 	Content string `json:"content" form:"content" validate:"required"`
--- cmd/main.go
+++ cmd/main.go
@@ -1,4 +1,4 @@
-package bootstrap
+package main
 
 import (
 	"context"
@@ -7,15 +7,21 @@ import (
 	"strings"
 	"time"
 
+	"go.uber.org/fx"
+
 	"github.com/efectn/fiber-boilerplate/app/middleware"
+	"github.com/efectn/fiber-boilerplate/app/module/article"
 	"github.com/efectn/fiber-boilerplate/app/router"
+	"github.com/efectn/fiber-boilerplate/internal/bootstrap"
 	"github.com/efectn/fiber-boilerplate/internal/database"
 	"github.com/efectn/fiber-boilerplate/utils/config"
 	"github.com/efectn/fiber-boilerplate/utils/response"
+	fxzerolog "github.com/efectn/fx-zerolog"
 	"github.com/gofiber/fiber/v2"
 	futils "github.com/gofiber/fiber/v2/utils"
 	"github.com/rs/zerolog"
-	"go.uber.org/fx"
+	"github.com/rs/zerolog/log"
+	_ "go.uber.org/automaxprocs"
 )
 
 func NewFiber(cfg *config.Config) *fiber.App {
@@ -121,4 +127,25 @@ func Start(lifecycle fx.Lifecycle, cfg *config.Config, fiber *fiber.App, router 
 			},
 		},
 	)
+}
+
+func main() {
+	fx.New(
+		// Provide patterns
+		fx.Provide(NewFiber),
+		fx.Provide(config.NewConfig),
+		fx.Provide(bootstrap.NewLogger),
+		fx.Provide(database.NewDatabase),
+		fx.Provide(middleware.NewMiddleware),
+		fx.Provide(router.NewRouter),
+
+		// Provide modules
+		article.NewArticleModule,
+
+		// Start Application
+		fx.Invoke(Start),
+
+		// Define logger
+		fx.WithLogger(fxzerolog.Init(log.Logger)),
+	).Run()
 }
